26901aaf3f0969f2e09a420ec47cd3d39076735b
Andrius <andrius@Mac.lan>
2026-04-23 09:22:13 +0300

    fix: postcss config for vercel deploy

--- apps/frontend/postcss.config.js
+++ apps/frontend/postcss.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-const rootModules = path.resolve(__dirname, '../../node_modules');
-
 module.exports = {
   plugins: {
-    [path.join(rootModules, 'tailwindcss')]: {},
-    [path.join(rootModules, 'autoprefixer')]: {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
